140e2ebeaf1a53bb4c6a2129f22eab6d986de69c
Emil <epniedzwiedz01@educarex.es>
2023-11-30 14:27:59 +0100

    Estoy en la rama1

--- src/paquete/HolaMundo.java
+++ src/paquete/HolaMundo.java
@@ -6,7 +6,7 @@ public class HolaMundo {
 		System.out.println("Hola mundo");
 		System.out.println("Buenos días");
 		System.out.println("Estoy subiendo esto al github");
-		System.out.println("Estoy en la rama 1");
+		System.out.println("Estoy en la rama1");
 	}
 
 }
